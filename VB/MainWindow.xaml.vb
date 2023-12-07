bf82339318ba10c7a40d998896e08e288e52e43b
DevExpressExampleBot <fakeEmail>
2023-12-07 15:17:30 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,4 +1,4 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
 Imports DevExpress.Xpf.Editors
 Imports DevExpress.Xpf.Grid
 Imports System.Collections.Generic
@@ -7,48 +7,51 @@ Imports System.Linq
 Imports System.Windows
 
 Namespace DXSample
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Property Items() As ObservableCollection(Of Item)
-		Public Sub New()
-			Items = New ObservableCollection(Of Item) From {
-				New Item With {.Value = "A"},
-				New Item With {.Value = "B"},
-				New Item With {.Value = "C"}
-			}
-			DataContext = Me
-			InitializeComponent()
-		End Sub
-		Private ActiveEditor As AutoSuggestEdit
-		Private Sub ShownEditor(ByVal sender As Object, ByVal e As EditorEventArgs)
-			ActiveEditor = TryCast(e.Editor, AutoSuggestEdit)
-			If ActiveEditor IsNot Nothing Then
-				AddHandler ActiveEditor.QuerySubmitted, AddressOf QuerySubmitted
-			End If
-		End Sub
-		Private Sub HiddenEditor(ByVal sender As Object, ByVal e As EditorEventArgs)
-			If ActiveEditor IsNot Nothing Then
-				RemoveHandler ActiveEditor.QuerySubmitted, AddressOf QuerySubmitted
-				ActiveEditor.ItemsSource = Nothing
-				ActiveEditor = Nothing
-			End If
-		End Sub
-		Private Sub QuerySubmitted(ByVal sender As Object, ByVal e As AutoSuggestEditQuerySubmittedEventArgs)
-			ActiveEditor.ItemsSource = New List(Of String)(Enumerable.Range(0, 10).Select(Function(c) e.Text & " item #" & c))
-		End Sub
-	End Class
-	Public Class Item
-		Inherits BindableBase
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private _Value As String
-		Public Property Value() As String
-			Get
-				Return _Value
-			End Get
-			Set(ByVal value As String)
-				SetProperty(_Value, value, "Value")
-			End Set
-		End Property
-	End Class
+        Public Property Items As ObservableCollection(Of Item)
+
+        Public Sub New()
+            Items = New ObservableCollection(Of Item) From {New Item With {.Value = "A"}, New Item With {.Value = "B"}, New Item With {.Value = "C"}}
+            DataContext = Me
+            Me.InitializeComponent()
+        End Sub
+
+        Private ActiveEditor As AutoSuggestEdit
+
+        Private Sub ShownEditor(ByVal sender As Object, ByVal e As EditorEventArgs)
+            ActiveEditor = TryCast(e.Editor, AutoSuggestEdit)
+            If ActiveEditor IsNot Nothing Then AddHandler ActiveEditor.QuerySubmitted, AddressOf QuerySubmitted
+        End Sub
+
+        Private Sub HiddenEditor(ByVal sender As Object, ByVal e As EditorEventArgs)
+            If ActiveEditor IsNot Nothing Then
+                RemoveHandler ActiveEditor.QuerySubmitted, AddressOf QuerySubmitted
+                ActiveEditor.ItemsSource = Nothing
+                ActiveEditor = Nothing
+            End If
+        End Sub
+
+        Private Sub QuerySubmitted(ByVal sender As Object, ByVal e As AutoSuggestEditQuerySubmittedEventArgs)
+            ActiveEditor.ItemsSource = New List(Of String)(Enumerable.Range(0, 10).[Select](Function(c) e.Text & " item #" & c))
+        End Sub
+    End Class
+
+    Public Class Item
+        Inherits BindableBase
+
+        Private _Value As String
+
+        Public Property Value As String
+            Get
+                Return _Value
+            End Get
+
+            Set(ByVal value As String)
+                SetProperty(_Value, value, "Value")
+            End Set
+        End Property
+    End Class
 End Namespace
